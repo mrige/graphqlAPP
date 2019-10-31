770b8e5ff1c5f9039566a8f6f976cf465bc7b7a0
Ayobami Ige <thorbaba01@gmail.com>
2019-10-31 09:01:08 -0500

    added mlab schema implementation

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const graphqlHTTP = require('express-graphql')
 const schema = require("./schema/schema")
+const mongoose = require('mongoose');
 
+mongoose.connect("mongodb+srv://ayobami:Olympus1@graphqlcluster-ccqdl.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser : true, useUnifiedTopology: true})
+mongoose.connection.once('open', () => {
+    console.log("connected to the database")
+})
 const app = express();
 
 app.use ('/graphql', graphqlHTTP({
